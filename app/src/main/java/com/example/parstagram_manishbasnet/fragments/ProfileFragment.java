3f693a79d231011a6b65d3b0bf76eba5798812d0
manishfsb <mnshbsnt@gmail.com>
2020-10-13 23:18:25 -0400

    Final Changes

--- app/src/main/java/com/example/parstagram_manishbasnet/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/parstagram_manishbasnet/fragments/ProfileFragment.java
@@ -1,4 +1,41 @@
-package com.example.parstagram_manishbasnet;
+package com.example.parstagram_manishbasnet.fragments;
 
-public class ProfileFragment {
+import android.util.Log;
+
+import com.example.parstagram_manishbasnet.Post;
+import com.example.parstagram_manishbasnet.fragments.PostsFragment;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostsFragment {
+    @Override
+    protected void queryPosts() {
+
+        super.queryPosts();
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addAscendingOrder(Post.KEY_CREATED_KEY);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username:" + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
 }
